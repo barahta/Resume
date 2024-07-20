3947672bac9c57efc46dad2512fb2fc7bf7a1093
Vladimir Barakhtyanskiy <barahtasurgut@gmail.com>
2024-07-21 02:45:00 +0500

    1234565

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React, {useContext, useEffect} from "react"
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
-
+import '@fortawesome/fontawesome-free/css/all.css';
 import {observer} from "mobx-react-lite";
 import './assets/styles/index.scss'
 import MainPage from "./pages/MainPage";
 import {DataProvider} from "./context/DataContext";
-
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {fas} from "@fortawesome/free-solid-svg-icons";
+import {fab} from "@fortawesome/free-brands-svg-icons";
+library.add(fas, fab);
 function App() {
+
   return (
       <DataProvider>
         <Router>
--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -1,10 +1,68 @@
 import {useState,useContext} from "react";
-
+import style from './styles/MainStyle.module.scss'
+import './styles/hovers.scss'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 export default function MainPage(){
 
+    const actionIcon = (icon) => {
+       return (icon)?true:false
+    }
+
     return (
-        <div className='container'>
-Привет
+        <div className={style.container}>
+            <div className={style.header}>
+                <div className={style.header_logo}>iamwelcome.ru</div>
+                <div className={style.header_menu}>
+                    <div className={style.header_menu_btn}>home</div>
+                    <div className={style.header_menu_btn}>about</div>
+                    <div className={style.header_menu_btn}>projects</div>
+                    <div className={style.header_menu_btn}>contact</div>
+                </div>
+            </div>
+            <div className={style.mainup}>
+                <div className={style.mainup_stack}>
+                    <i className="fa-brands fa-html5 html5"/>
+                    <i className="fa-brands fa-css3-alt css3"/>
+                    <i className="fa-brands fa-sass sass"/>
+                    <i className="fa-brands fa-node-js js"/>
+                    <i className="fa-brands fa-react react"/>
+                </div>
+                <div className={style.mainup_vizitka}>
+                    <div className={style.mainup_vizitka_left}>
+                        <div className={style.mainup_vizitka_left_title}>FRONT-END REACT DEVELOPER</div>
+                        <div className={style.mainup_vizitka_left_name}>I’m Vladimir Alekseevich Barakhtyansliy</div>
+                        <div className={style.mainup_vizitka_left_hello}>
+                            A passionate front-end developer
+                            from Surgut, Russia,
+                            looking to relocate to your city
+                        </div>
+                    </div>
+                    <div className={style.mainup_vizitka_right}>
+                        <div className={style.mainup_vizitka_right_photo}></div>
+                        <div className={style.mainup_vizitka_right_mask}></div>
+                    </div>
+                </div>
+            </div>
+            <div className={style.reversesblocks}>
+                <div className={style.revers_btn_age}>Age</div>
+                <div className={style.revers_btn_level}>Educa
+                    tion
+                    Level</div>
+                <div className={style.revers_btn_position}>Current Position</div>
+                <div className={style.revers_btn_contact}>Contact</div>
+            </div>
+            <div className={style.experience}>
+                <div className={style.experience_title}>Work Experience</div>
+                <div className={style.experience_history}>
+                    <div className={style.experience_history_left}></div>
+                    <div className={style.experience_history_center}></div>
+                    <div className={style.experience_history_right}></div>
+                </div>
+            </div>
+            <div className={style.portfolio}></div>
+            <div className={style.aboutme}></div>
+            <div className={style.grabform}></div>
+            <div className={style.contact}></div>
         </div>
     );
 };
